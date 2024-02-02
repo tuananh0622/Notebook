da622af4164ac6594d2b465206616d5b959acfba
tuananh0622 <135480892+tuananh0622@users.noreply.github.com>
2024-02-02 02:46:05 +0000

    ok

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,40 @@
-print("What is your freaking name?")
-myName = input()
-print("Great to see you, " + myName)
-# length = str(len(myName))
-print("The length of your name is: " + str(len(myName)))
-print("What is your age?")
-myAge = input()
-print("Your age will be " + str(int(myAge) + 1) + " next year")
+# print("What is your freaking name?")
+# myName = input()
+# print("Great to see you, " + myName)
+# # length = str(len(myName))
+# lengthmyName = str(len(myName))
+# print("The length of your name is: " + lengthmyName)
+# print("What is your age?")
+# myAge = input()
+# print("Your age will be " + str(int(myAge) + 1) + " next year")
+# spam = int(myAge)
+# while spam < 25:
+#     print("You are too young")
+#     spam = spam + 2
+
+# print("What is your name?")
+# name = input()
+# while name!= str:
+#     print("Please write your name!")
+# Create a loop function suggests about writing your name
+# name = ''
+# while True:
+#     print("Please write down your name!")
+#     name = input()
+#     if name == 'your name':
+#         break
+# print("Thank you")
+
+# spam = 0
+# while spam < 5:
+#     spam = spam + 1
+#     if spam == 3:
+#         continue
+#     print("Spam is " + str(spam))
+
+# print("My name is ")
+# for i in range(2, 25):
+#     print("Hi, pick me up" + str(i))
+
+import random
+random.randint(1, 10)
